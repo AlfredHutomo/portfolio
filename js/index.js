3b86c5e0e791696cadd0647bd5b21daf4e898097
AlfredHutomo <alfredhutomo@gmail.com>
2020-10-27 19:50:14 +1100

    Update on coming soon page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var options = {
+	strings: ["Coding now 💻^1000", "Drinking coffee ☕^1000", "Playing games 🎮^1000"],
+	typeSpeed: 70,
+	backSpeed: 50,
+	loop: true,
+};
+
+var typed = new Typed(".type", options);
