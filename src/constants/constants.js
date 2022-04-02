de894b75fc0f030f3afaa449f619b30bb7ad52f1
AlfredHutomo <alfredhutomo@gmail.com>
2022-04-02 21:20:57 +0700

    modify new projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,50 +1,50 @@
 export const projects = [
-  {
-    title: 'Quiz Plus',
-    description:
-      'This is a project for final challenge in the Rapid Application Development class I took in RMIT University, which all the student must complete this app within 24 Hours applying our knowledge in RoR and Client API usage.',
-    image: '/images/quiz-plus.png',
-    tags: ['Ruby On Rails'],
-    source: '',
-    visit: 'https://quiz-plus-s3790172.herokuapp.com/',
-    id: 0,
-  },
-  {
-    title: 'Aneka Makmur Website',
-    description:
-      'This is a re-design of the company website of Aneka Makmur. This website fully utilise a headless cms (Strapi) to create each pages and edit contents. This website is still under development.',
-    image: '/images/anekamakmur.png',
-    tags: ['Next.js', 'tailwind.css', 'Strapi', 'Node.js'],
-    source: '',
-    visit: '',
-    id: 3,
-  },
-  {
-    title: 'Portfolio',
-    description:
-      "This website is my first portfolio website that I have created. This is also my first time using the Next.js framework which actually I enjoyed working on because it's React basically.",
-    image: '/images/portfolio.png',
-    tags: ['React', 'Next.js', 'Vercel'],
-    source: 'https://github.com/AlfredHutomo/portfolio',
-    visit: '/',
-    id: 1,
-  },
-  {
-    title: 'IFF Australia Website',
-    description:
-      'This project is co-worked with a web developer from Relab Studio, Melbourne. In this project, I worked on creating pages and worked on optimizing the design by following design provided from the design team in Relab Studio. This is the first project I worked with Gatsby.js.',
-    image: '/images/iff.png',
-    tags: ['React', 'Gatsby.js', 'Shopify'],
-    source: '',
-    visit: 'https://iffaustralia.com',
-    id: 1,
-  },
+    {
+        title: 'Real Futbol Institute',
+        description:
+            "This is a sponsor project of Relab Studios, Melbourne. In this project, I did R&D of the headless cms solution for this client project. I worked with Relab's lead developer to create this headless cms experience.",
+        image: '/images/rfi.png',
+        tags: ['Next.js', 'Strapi', 'GraphQL', 'Vercel', 'DigitalOcean', 'AWS'],
+        source: '',
+        visit: 'https://www.realfutbolinstitute.com/',
+        id: 0,
+    },
+    {
+        title: 'Quiz Plus',
+        description:
+            'This is a project for final challenge in the Rapid Application Development class I took in RMIT University, which all the student must complete this app within 24 Hours applying our knowledge in RoR and Client API usage.',
+        image: '/images/quiz-plus.png',
+        tags: ['Ruby On Rails'],
+        source: '',
+        visit: 'https://quiz-plus-s3790172.herokuapp.com/',
+        id: 1,
+    },
+    {
+        title: 'Portfolio',
+        description:
+            "This website is my first portfolio website that I have created. This is also my first time using the Next.js framework which actually I enjoyed working on because it's React basically.",
+        image: '/images/portfolio.png',
+        tags: ['React', 'Next.js', 'Vercel'],
+        source: 'https://github.com/AlfredHutomo/portfolio',
+        visit: '/',
+        id: 2,
+    },
+    {
+        title: 'IFF Australia Website',
+        description:
+            'This project is co-worked with a web developer from Relab Studios, Melbourne. In this project, I worked on creating pages and worked on optimizing the design by following design provided from the design team in Relab Studio. This is the first project I worked with Gatsby.js.',
+        image: '/images/iff.png',
+        tags: ['React', 'Gatsby.js', 'Shopify'],
+        source: '',
+        visit: 'https://iffaustralia.com',
+        id: 3,
+    },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey in computing' },
-  { year: 2018, text: 'Studied engineering in Monash College' },
-  { year: 2019, text: 'Started freelance web development' },
-  { year: 2020, text: 'Study info-tech in RMIT University' },
-  { year: 2021, text: 'Creating more useful and meaningful websites' },
+    { year: 2017, text: 'Started my journey in computing' },
+    { year: 2018, text: 'Studied engineering in Monash College' },
+    { year: 2019, text: 'Started freelance web development' },
+    { year: 2020, text: 'Study info-tech in RMIT University' },
+    { year: 2021, text: 'Become a Full Stack developer' },
 ];
